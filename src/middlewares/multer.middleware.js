b96c76cc8a52bf403ea563e541853b801ef55fe1
Praphul-whiz <arun@whiz-cloud.com>
2024-02-09 19:27:20 +0530

    middleware-filehandlind

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname ) ///better to use unique name
+    }
+  })
+  
+ export const upload = multer({ storage })
