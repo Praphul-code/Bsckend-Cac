26dd3ec7af6ee922fc12ce2ba5a5e00175b2fd04
Praphul-whiz <arun@whiz-cloud.com>
2024-02-10 22:31:18 +0530

    postman and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,7 +14,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields is required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -23,17 +23,18 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
+    console.log(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if (!avatar) {
-        throw new ApiError(400, "Avatar file is required")
+        throw new ApiError(400, "Avatar  is required")
     }
 
    const user = await User.create({
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -19,8 +19,10 @@ const uploadOnCloudinary = async (localFilePath) => {
             })
         
             console.log('File uploaded successfully',response.url);
+            fs.unlinkSync(localFilePath)
             return response;
     } catch (error) {
+        console.log('File uploaded successfully',error);
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file
         return null
     }
