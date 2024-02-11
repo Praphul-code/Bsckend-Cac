ae0a0f0a0b0823cb9c3d7479ca17965aabe38a87
Praphul-whiz <arun@whiz-cloud.com>
2024-02-11 18:58:26 +0530

    access-refresh-token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,18 +3,19 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
 
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
     const user = await User.findById(userId)
-    const accesToken = user.generateAccessToken()
+    const accessToken = user.generateAccessToken()
     const refreshToken = user.generateRefreshToken()
 
     user.refreshToken = refreshToken
     await user.save({ validateBeforeSave: false })
 
-    return { accesToken, refreshToken }
+    return { accessToken, refreshToken }
 
   } catch (error) {
     throw new ApiError(500, "Something went wrong while generating refresh and access token")
@@ -22,7 +23,7 @@ const generateAccessAndRefreshTokens = async (userId) => {
 }
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body
-  console.log("email", email)
+
 
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -46,7 +47,6 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatar = await uploadOnCloudinary(avatarLocalPath)
-  console.log(avatarLocalPath)
   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
   if (!avatar) {
@@ -77,58 +77,66 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 
-const loginUser = asyncHandler(async (req, res) => {
+const loginUser = asyncHandler(async (req, res) =>{
   // req body -> data
   // username or email
-  // find the user
-  // password check
-  // access and refresh token
-  // send cookie
+  //find the user
+  //password check
+  //access and referesh token
+  //send cookie
 
-  const { username, email, password } = req.body
+  const {email, username, password} = req.body
+  console.log(email);
 
   if (!username && !email) {
-    throw new ApiError(400, "username or email is required")
-
+      throw new ApiError(400, "username or email is required")
   }
+  
+  // Here is an alternative of above code based on logic discussed in video:
+  // if (!(username || email)) {
+  //     throw new ApiError(400, "username or email is required")
+      
+  // }
 
   const user = await User.findOne({
-    $or: [{ username }, { email }]
+      $or: [{username}, {email}]
   })
 
   if (!user) {
-    throw new ApiError(404, "User does not exist")
+      throw new ApiError(404, "User does not exist")
   }
 
-  const isPasswordValid = await user.isPasswordCorrect(password)
+ const isPasswordValid = await user.isPasswordCorrect(password)
 
-
-  if (!isPasswordValid) {
-    throw new ApiError(401, "Invalid User Credentials")
+ if (!isPasswordValid) {
+  throw new ApiError(401, "Invalid user credentials")
   }
 
-  const { accesToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
+ const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
 
   const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
   const options = {
-    httpOnly: true,
-    secure: true
+      httpOnly: true,
+      secure: true
   }
 
-  return res.status
-    .cookie("accessToken", accesToken, options)
-    .cookie("refreshToken", refreshToken, options)
-    .json(
+  return res
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", refreshToken, options)
+  .json(
       new ApiResponse(
-        200,
-        {
-          user: loggedInUser, accesToken, refreshToken
-        },
-        "User logged In Succesfully"
+          200, 
+          {
+              user: loggedInUser, accessToken, refreshToken
+          },
+          "User logged In Successfully"
       )
-    )
+  )
+
 })
+
 
 const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
@@ -150,8 +158,39 @@ const logoutUser = asyncHandler(async (req, res) => {
   .json(new ApiResponse(200, {}, "User logged Out"))
 })
 
+const refreshAccessToken = asyncHandler(async (req, res) => {
+ const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+ if(!incomingRefreshToken){
+  throw new ApiError(400,"unathorized request")
+ }
+
+try {
+   const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+  
+   const user = await User.findById(decodedToken?._id)
+   if(!user){
+    throw new ApiError(400,"Invalid refresh token")
+   }
+  
+   if(incomingRefreshToken !== user?.refreshToken){
+    throw new ApiError(401, "Refresh token is expired or used")
+   }
+  
+   const options ={
+    httpOnly: true,
+    secure: true
+   }
+  
+  const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+   return res.status(200).cookie("accessToken", accessToken).cookie("refresh", newRefreshToken).json(
+    new ApiResponse(200, accessToken, newRefreshToken, "Access token refreshed")
+   )
+} catch (error) {
+  throw new ApiError(401, error?.message || "Invalid refresh token")
+}
+} )
 
 
 
-
-export { registerUser, loginUser, logoutUser }
+export { registerUser, loginUser, logoutUser , refreshAccessToken }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 import { upload } from "../middlewares/multer.middleware.js"
 const router = Router()
@@ -16,6 +17,13 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+router.route("/login").post(
+    loginUser)
+
+router.route("/logout").post(verifyJWT, logoutUser)
+
+router.route("./refresh-token").post(refreshAccessToken)
 
 
 export default router
